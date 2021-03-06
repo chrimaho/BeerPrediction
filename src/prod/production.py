7280ac29719a70f0b39cac273290007ec6f32658
chrimaho <chrismahoney@hotmail.com>
2021-03-06 15:51:57 +1100

    Add predict_single()

--- src/prod/production.py
+++ src/prod/production.py
@@ -58,3 +58,58 @@ def read_html(path:str="./docs/info.html"):
         data = file.read()
     return data
 
+
+
+#------------------------------------------------------------------------------#
+#                                                                              #
+#    /beer/type                                                             ####
+#                                                                              #
+#------------------------------------------------------------------------------#
+
+def predict_single \
+    ( brewery_name:str="Epic Ales"
+    , review_aroma:float=1
+    , review_appearance:float=1
+    , review_palate:float=1
+    , review_taste:float=1
+    , modl_path:str="./models/predictors/beer_prediction.pth"
+    ):
+    
+    # Imports
+    from src.utils import assertions as a
+    from src.models.predict import prepare_data, predict_classification, decode_predictions
+    from src.models.pytorch import Net
+    import torch
+    # from joblib import load
+    
+    # Assertions
+    assert a.all_str(brewery_name)
+    assert all([a.all_float_or_int(param) for param in [review_aroma, review_appearance, review_palate, review_taste]])
+    assert a.all_str(modl_path)
+    assert a.all_valid_path(modl_path)
+    
+    # Loads
+    # modl = load(modl_path)
+    modl = Net(5, 104)
+    modl.load_state_dict(torch.load(modl_path))
+
+    
+    # Prepare data
+    data = prepare_data \
+        ( brewery_name=brewery_name
+        , review_aroma=review_aroma
+        , review_appearance=review_appearance
+        , review_palate=review_palate
+        , review_taste=review_taste
+        )
+        
+    # Predict data
+    data = predict_classification(data, modl)
+    
+    # Decode data
+    data = decode_predictions(data)
+    
+    # Flatten
+    data = data.flatten()
+        
+    return data
